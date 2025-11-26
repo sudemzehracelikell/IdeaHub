625c2a48cb25d634e50f2abaafb2604631c3b1d0
Yusuf-Sefa-Laptop <yusuf.artar@istun.edu.tr>
2025-11-26 20:58:10 +0300

    Last UserApp and TeamApp model files changes

--- UserApp/models.py
+++ UserApp/models.py
@@ -2,16 +2,16 @@ from enum import Enum
 from django.db import models
 
 class Roles(models.IntegerChoices):
-    ADMIN = 1
-    PARTICIPANT = 2
-    TEAMLEADER = 3
-    JUDGE = 4
+    ADMIN = 1, 'Admin'
+    PARTICIPANT = 2, 'Partipicant'
+    TEAMLEADER = 3, 'Team Leader'
+    JUDGE = 4, 'Judge'
 
 class User(models.Model):
     userName = models.CharField(max_length=50)
     email = models.EmailField()
     password = models.CharField(max_length=50)
-    role = models.IntegerField(choices=Roles.choices, default=Roles.PARTICIPANT)
+    role = models.IntegerField(choices=Roles.choices, default=Roles.PARTICIPANT, verbose_name='User Role')
 
     def __str__(self):
         return self.userName
